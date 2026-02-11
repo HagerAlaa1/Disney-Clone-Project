8fc6a0fb3a6787ef1bb22b88e3ce701af5189532
HagerAlaa1 <140528065+HagerAlaa1@users.noreply.github.com>
2026-02-11 13:34:07 +0200

    refactor: remove unused Suspense component and fix import statement

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
-import { StrictMode, Suspense } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
-import "./utils/i18n/I18N.ts"
+import "./utils/i18n/I18N.ts";
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <StrictMode>
-      {/* <Suspense fallback = {<div>Loading...</div>}> */}
       <App />
-      {/* </Suspense> */}
     </StrictMode>
-  </BrowserRouter>
+  </BrowserRouter>,
 );
